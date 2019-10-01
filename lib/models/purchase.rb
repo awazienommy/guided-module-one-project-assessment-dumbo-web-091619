8869d846ef0dac38fa429c2c61e237d05a95a422
Lyle Dickie <lyle.e.dickie@gmail.com>
2019-10-01 14:10:05 -0400

    Create class methods to create new tax and purchase instances

--- lib/models/purchase.rb
+++ lib/models/purchase.rb
@@ -1,4 +1,11 @@
 class Purchase < ActiveRecord::Base
-    belongs_to :customers
-    belongs_to :companies
+  belongs_to :customers
+  belongs_to :companies
+
+  def self.new_purchase(company_instance, customer_instance, purchase_amount)
+    Purchase.create(company_id: company_instance.id, customer_id: customer_instance.id, purchase_amount: purchase_amount)
+  end
+
+
+  
 end
--- lib/models/tax.rb
+++ lib/models/tax.rb
@@ -2,5 +2,10 @@ class Tax < ActiveRecord::Base
   belongs_to :governments
   belongs_to :companies
 
+  def self.new_tax(government_instance, company_instance, amount)
+    # government_id company_id amount
+    Tax.create(government_id: government_instance.id, company_id: company_instance.id, amount: amount)
+  end
+
 
 end
