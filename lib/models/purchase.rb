adf2f400c8efd35fca3e414994927d6f9198544e
awazienommy <coawazie@gmail.com>
2019-10-07 07:10:35 -0400

    Clean up taxes

--- lib/models/purchase.rb
+++ lib/models/purchase.rb
@@ -15,27 +15,4 @@ class Purchase < ActiveRecord::Base
     puts "-----Purchase-----" "Product: #{product_instance.name}" "Quantity: #{quantity}" "Company: #{company.name}" "Amount: #{purchase_amount}"
     Interface.customer_main_menu
   end
-
-
-  # def self.get_refund
-  #   customer_name_input = @@prompt.ask("Welcome to refunds. What is your name?")
-  #   customer_instance = Customer.find_by(name: customer_name_input)
-  #   purchases = Purchase.select(customer_instance: customer_instance.id)
-  #   purchases_names = purchases.map {|purchase| purchase.name}
-  #   selected_purchase_name = @@prompt.select("What which purchase do you want a refund for?", purchases_names, per_page: 10)
-  #   selected_purchase_instance = purchases.find_by(name: selected_purchase_name)
-  #   if selected_purchase_instance_company_instance.refund
-  #     puts "Sorry, but you have already got a refund for this purchase"
-  #   else
-  #     selected_purchase_instance_company_instance = Company.find_by(id: selected_purchase_instance.company_id)
-  #     selected_purchase_instance.update(refunded: true)
-  #     new_customer_balance = customer_instance.balance + selected_purchase_instance.amount
-  #     new_company_balance = selected_purchase_instance.amount + selected_purchase_instance_company_instance.balance
-  #     customer_instance.update(balance: new_customer_balance)
-  #     selected_purchase_instance_company_instance.update(balance: new_company_balance)
-  #     puts "Your refund for $#{selected_purchase_instance.amount} has been processed. This should reflect on your balance"
-  #   end
-  #   Interface.customer_main_menu
-  # end
-
 end
--- lib/models/tax.rb
+++ lib/models/tax.rb
@@ -2,26 +2,4 @@ class Tax < ActiveRecord::Base
   belongs_to :government
   belongs_to :company
 
-  def self.new_tax(government_instance, company_instance, amount)
-    # government_id company_id amount
-    # create new tax object
-    Tax.create(government_id: government_instance.id, company_id: company_instance.id, amount: amount)
-  end
-
-
-  def self.amount_of_tax_by_industry(industry_string)
-    # calculate total tax for passed in industry name
-    companies = Company.all.filter { |company| company.industry == industry_string }
-    companies.map { |company| company.taxes }[0].map { |tax| tax.amount }.sum
-  end
-
-
-
-
-
-
-
-
-
-
 end
